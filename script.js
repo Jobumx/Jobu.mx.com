6bbbb2a47c5cabdb705922d056d7c3b90f378736
Jobumx <146441375+Jobumx@users.noreply.github.com>
2023-09-29 14:07:17 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 // script.js
-
-// Efecto de paralaje en la sección de héroe
-document.addEventListener("scroll", function () {
-  const scrollY = window.scrollY;
-  const hero = document.querySelector(".hero");
-
-  // Ajusta la posición de fondo de la sección de héroe
-  hero.style.backgroundPositionY = -scrollY * 0.5 + "px";
-});
